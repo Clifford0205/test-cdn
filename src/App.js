072c53db3f0eb58988672b0cc471267a09999a47
hotspring666 <hotspring666@protonmail.com>
2023-07-11 18:07:20 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import "./App.css";
+import { IconButton, Popover, Toolbar, AppBar } from "@mui/material";
+import icon from "./ic_widjet.svg";
+
+function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  return (
+    <>
+      <IconButton
+        onClick={handleClick}
+        style={{ position: "absolute", left: 20, bottom: 20 }}
+      >
+        <img src={icon} style={{ width: 60, height: 60 }} />
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <div style={{ height: 520, overflow: "auto", width: 320 }}>
+          <AppBar position="static">
+            <Toolbar>Notifications</Toolbar>
+          </AppBar>
+        </div>
+      </Popover>
+    </>
+  );
+}
+
+export default App;
